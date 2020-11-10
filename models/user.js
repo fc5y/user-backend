fd8db07d58363233fdabdc35bd1449a71850e533
San Cao <caothesan@gmail.com>
2020-11-10 22:09:10 +0700

    Update user database

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const { DataTypes } = require('sequelize');
+const { Sequelize } = require('../sequelize');
 
 // We export a function that defines the model.
 // This function will automatically receive as parameter the Sequelize connection object.
@@ -15,6 +16,7 @@ module.exports = (sequelize) => {
 		email: {
 			allowNull: false,
 			type: DataTypes.STRING,
+			defaultValue: "abc@xyz.com",
 			unique: true,
 			validate: {
 				isEmail: true
@@ -23,6 +25,7 @@ module.exports = (sequelize) => {
 		full_name: {
 			type: DataTypes.STRING,
 			allowNull: false,
+			defaultValue: "Nguyen Van A"
 		},
 		school_name: {
 			type: DataTypes.STRING,
@@ -31,14 +34,29 @@ module.exports = (sequelize) => {
 		password: { // hashed password have length of 64
 			type: DataTypes.STRING(64),
 			unique: true,
+			defaultValue: "MaLhr2g3uWUuUu57FQn2xGPJ6k24UaVnY6amEuk8BvchStnqkNe9o9W5s6CnXLox",
 			allowNull: false,
 			validate: {
 				is: /^[0-9a-zA-Z]{64}$/i
 			}
 		},
 		avatar_url: {
-			type: DataTypes.STRING,
+			type: DataTypes.STRING
 		},
-
+		is_email_verified: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+		}
+	}),
+	sequelize.define('rating_histories', {
+		user_id: {
+			allowNull: false,
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'users',
+				key: 'id'
+			}
+		}
 	});
 };
