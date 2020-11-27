112e7d01dc5e204972ae972f902fdbcce1da4c90
pythagore1123 <ledinhhai1312@gmail.com>
2020-11-27 22:17:14 +0700

    Fix lint

--- migrations/20201124134824-modify_users_add_username.js
+++ migrations/20201124134824-modify_users_add_username.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
     await queryInterface.sequelize.query(
-      "ALTER TABLE Users ADD COLUMN username VARCHAR(50) NOT NULL UNIQUE DEFAULT(email) AFTER id;"
+      "ALTER TABLE Users ADD COLUMN username VARCHAR(50) NOT NULL UNIQUE;"
     );
   },
 
