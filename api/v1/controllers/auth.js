ba86cfeeae26346a5b212e76c1edb67ab63b2843
pythagore1123 <ledinhhai1312@gmail.com>
2020-12-12 15:21:30 +0700

    Migrate auth to v1

---
+++ api/v1/controllers/auth.js
@@ -0,0 +1,189 @@
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+const db = require("../models/index.js");
+const models = db.sequelize.models;
+const { statusCode, emailRegex } = require("../utils");
+const errors = require("../utils/error");
+const { buildUserJson } = require("./user");
+
+const SALT_ROUNDS = 10;
+const bcrypt = require("bcryptjs");
+const otpGenerator = require("otp-generator");
+const { updateOrCreate } = require("../utils/models.js");
+const { createMessage, sendMail } = require("../utils/email-sender");
+
+function isEmail(email_or_username) {
+  return emailRegex.test(email_or_username);
+}
+
+function sanitizeUserDetails(data) {
+  const salt = bcrypt.genSaltSync(SALT_ROUNDS);
+  const hashedPassword = bcrypt.hashSync(data.password, salt);
+  let userDetails = {
+    username: data.username,
+    full_name: data.full_name,
+    email: data.email,
+    school_name: data.school_name,
+    password: hashedPassword,
+    avatar: data.avatar,
+    is_email_verified: data.is_email_verified,
+  };
+  return userDetails;
+}
+
+async function login(req, res) {
+  if (req.user) {
+    return res.status(statusCode.SUCCESS).json({
+      code: 0,
+      msg: "Already logged in",
+      data: {
+        access_token: jwt.sign(
+          {
+            email: req.user.email,
+            id: user.id,
+          },
+          process.env.JWT_SECRET,
+        ),
+      },
+    });
+  }
+
+  const { email_or_username, password } = req.body;
+  if (!email_or_username || !password) {
+    throw new errors.FcError(errors.MISSING_REQUIRED_FIELDS);
+  }
+
+  let user;
+  if (isEmail(email_or_username)) {
+    user = await models.User.findOne({
+      where: {
+        email: email_or_username,
+      },
+    });
+  } else {
+    user = await models.User.findOne({
+      where: {
+        username: email_or_username,
+      },
+    });
+  }
+
+  if (!user) {
+    throw new errors.FcError(errors.EMAIL_USERNAME_PASSWORD_INVALID);
+  }
+
+  if (bcrypt.compareSync(password, user.password)) {
+    return res.status(statusCode.SUCCESS).json({
+      code: 0,
+      msg: "Login successful",
+      data: {
+        access_token: jwt.sign(
+          {
+            email: user.email,
+            id: user.id,
+          },
+          process.env.JWT_SECRET,
+        ),
+      },
+    });
+  }
+  throw new errors.FcError(errors.EMAIL_USERNAME_PASSWORD_INVALID);
+}
+
+async function sendOtp(req, res) {
+  if (!req.body.email) {
+    throw new errors.FcError(errors.MISSING_EMAIL);
+  }
+
+  if (await models.User.findOne({ where: { email: req.body.email } })) {
+    throw new errors.FcError(errors.EMAIL_EXISTS);
+  }
+
+  const otp = otpGenerator.generate(6, {
+    digits: true,
+    alphabets: false,
+    upperCase: false,
+    specialChars: false,
+  });
+  const now = new Date();
+  const expired_time = new Date(now.getTime() + 10 * 60000); // 10 minutes later
+  await updateOrCreate(
+    models.EmailVerification,
+    { email: req.body.email },
+    {
+      email: req.body.email,
+      otp: otp,
+      expired_time: expired_time,
+    },
+  );
+  const message = createMessage(
+    req.body.email,
+    "Email verification",
+    "OTP: " + otp,
+  );
+  await sendMail(message);
+  return res.status(statusCode.SUCCESS).send({
+    code: 0,
+    msg: "Sent OTP successfully",
+    data: {},
+  });
+}
+
+async function checkOtp(req) {
+  if (!req.body.email || !req.body.otp) {
+    throw new errors.FcError(errors.OTP_INVALID);
+  }
+  const emailVerification = await models.EmailVerification.findOne({
+    where: {
+      email: req.body.email,
+    },
+  });
+  if (!emailVerification || emailVerification.otp != req.body.otp) {
+    throw new errors.FcError(errors.OTP_INVALID);
+  }
+  const now = new Date();
+  if (emailVerification.expired_time < now) {
+    throw new errors.FcError(errors.OTP_EXPIRED);
+  }
+  return true;
+}
+
+async function releaseOtp(email) {
+  await models.EmailVerification.destroy({
+    where: { email: email },
+  });
+}
+
+async function signup(req, res) {
+  const { email, username, full_name, password, otp } = req.body;
+  if (!email || !username || !full_name || !otp || !password) {
+    throw new errors.FcError(errors.MISSING_REQUIRED_FIELDS);
+  }
+  if (await models.User.findOne({ where: { username } })) {
+    throw new errors.FcError(errors.USERNAME_EXISTS);
+  }
+
+  if (await models.User.findOne({ where: { email } })) {
+    throw new errors.FcError(errors.EMAIL_EXISTS);
+  }
+
+  // check OTP
+  await checkOtp(req);
+
+  await releaseOtp(email);
+
+  const user = await models.User.create(sanitizeUserDetails(req.body));
+
+  res.status(statusCode.SUCCESS).json({
+    code: 0,
+    msg: "Create user successful",
+    data: buildUserJson(user),
+  });
+}
+
+module.exports = {
+  login,
+  signup,
+  sendOtp,
+};
--- api/v1/index.js
+++ api/v1/index.js
@@ -1,9 +1,52 @@
 const express = require("express");
 const router = express.Router();
 
+const authController = require("../controller/auth");
+const { FcError, SYSTEM_ERROR } = require("../utils/error");
+const { statusCode } = require("../utils");
+const isNotUser = require("../middlewares/isNotUser");
+
 // GET /api/v1/hello
 router.get("/hello", (req, res) => {
   res.json({ msg: "Hello" });
 });
+
+function makeHandlerAwareOfAsyncErrors(handler) {
+  return async function (req, res, next) {
+    try {
+      await handler(req, res, next);
+    } catch (error) {
+      if (error instanceof FcError) {
+        res.status(statusCode.BAD_REQUEST).send({
+          code: error.code,
+          msg: error.msg,
+          data: error.data || {},
+        });
+      } else {
+        const fcError = new FcError(SYSTEM_ERROR);
+        res.status(statusCode.BAD_REQUEST).send({
+          code: fcError.code,
+          msg: error.msg,
+          data: fcError.data || {},
+        });
+      }
+    }
+  };
+}
+
+// Auth
+router.post(
+  "/login",
+  makeHandlerAwareOfAsyncErrors(authController.login),
+);
+router.post(
+  "/send-otp",
+  makeHandlerAwareOfAsyncErrors(authController.sendOtp),
+);
+router.post(
+  "/signup",
+  makeHandlerAwareOfAsyncErrors(isNotUser),
+  makeHandlerAwareOfAsyncErrors(authController.signup),
+);
 
 module.exports = router;
